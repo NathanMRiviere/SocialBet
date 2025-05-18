984d038242bbed9211365092f44381c0b7300558
Nathan Riviere <nathanriviere@Nathans-Laptop.local>
2025-05-18 03:01:22 -0400

    .

--- src/components/ListItemBody.tsx
+++ src/components/ListItemBody.tsx
@@ -5,50 +5,40 @@ import { LineChart } from '@mui/x-charts/LineChart';
 import { DataGrid } from '@mui/x-data-grid';
 import { PropData, AnalyticsData } from './types';
 
-const exampleData : AnalyticsData = {
-    lineData : {
-        timestamps : [1, 2, 3, 4, 5, 6],
-        lines : [255.5, 254.5, 254.5, 255.5, 255.5]
-    },
-    playerData : [{
-        home : false,
-        opponent : "",
-        result : 300
-    }]
+interface PropType {
+    analytics : AnalyticsData,
+    info : PropData,
+    loaded : boolean
 }
 
-const ListItemBody: React.FC<PropData> = (props) => {
-    const [data, setData] = useState(exampleData)
-    const [loaded, setLoaded] = useState(false)
-    useEffect(() => {
-        setTimeout(() => {
-            setLoaded(true)
-          }, 3000);
-        });
+const loadingContent = (
+    <>
+        <div className={styles.graphContainer}> 
+            <Skeleton variant="rectangular" width="100%" height={200} sx={{
+                marginBottom: "20px",
+            }} />
+            <Divider orientation='vertical' />
+            <Skeleton variant="rectangular" width="70%" height={300} />
+        </div>
+        <Skeleton variant="text"/>
+        <Skeleton variant="text"/>
+        <Skeleton variant="text"/>
+        <Skeleton variant="text"/>
+    </>
+)
+
+const ListItemBody: React.FC<PropType> = (props) => {
     return (
         <Container sx={{
             minHeight : "500px"
         }}>
-            {!loaded ? (
-                <>
-                    <div className={styles.graphContainer}> 
-                        <Skeleton variant="rectangular" width={400} height={300} sx={{
-                            marginRight: "20px",
-                        }} />
-                        <Divider orientation='vertical' />
-                        <Skeleton variant="rectangular" width={300} height={300} />
-                    </div>
-                    <Skeleton variant="text"/>
-                    <Skeleton variant="text"/>
-                    <Skeleton variant="text"/>
-                    <Skeleton variant="text"/>
-                </>
-            ) : (  
-                <LineChart
-                    xAxis={[{ data: data.lineData.timestamps, label : "timestamp"}]}
-                    series={[{data: data.lineData.lines, label: "line"}]}
-                    height={300}/>
-            )}
+            {!props.loaded ? loadingContent
+             : (  
+                    <LineChart
+                        xAxis={[{ data: props.analytics.lineData.timestamps, label : "timestamp"}]}
+                        series={[{data: props.analytics.lineData.lines, label: "line"}]}
+                        height={300}/>
+                )}
         </Container>
     )
 }
--- src/components/PropBetListItem.tsx
+++ src/components/PropBetListItem.tsx
@@ -1,15 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ListItem, ListItemIcon, ListItemText, Divider} from '@mui/material';
 import styles from "./BetList.module.css";
 import BetButtonGroup from './BetButtonGroup';
 import ListItemBody from './ListItemBody';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-import { PropData } from './types';
+import { PropData, AnalyticsData } from './types';
 
+const exampleData : AnalyticsData = {
+    lineData : {
+        timestamps : [1, 2, 3, 4, 5, 6],
+        lines : [255.5, 254.5, 254.5, 255.5, 255.5]
+    },
+    playerData : [{
+        home : false,
+        opponent : "",
+        result : 300
+    }]
+}
 
 const BetListItem: React.FC<PropData> = (props) => {
     const [expanded, setExpanded] = useState(false)
+    const [data, setData] = useState(exampleData)
+    const [loaded, setLoaded] = useState(false)
+    useEffect(() => {
+        setTimeout(() => {
+            setLoaded(true)
+          }, 3000);
+        });
+
     const header = (
         <ListItem key={props.id}>
             <ListItemIcon onClick={() => setExpanded(!expanded)}>
@@ -25,18 +44,16 @@ const BetListItem: React.FC<PropData> = (props) => {
         </ListItem>
     )
 
-    const body = expanded ? (
-        <>
-            <Divider orientation='horizontal' />
-            <ListItemBody {...props} />
-        </>
-        
-    ) : null
-
     return (
         <div className={styles.container} >
-            {header}   
-            {body}
+            {header}
+            { expanded && (
+                <>
+                    <Divider orientation='horizontal' />
+                    <ListItemBody info={props} analytics={data} loaded={loaded}/> 
+                </>
+            )}
+            
         </div>
     )
 }
